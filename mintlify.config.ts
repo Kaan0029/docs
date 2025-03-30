8cae9c6689a6ed3fd40432c09157e72f9f417570
Kaan0029 <ke2461@columbia.edu>
2025-03-29 20:56:03 -0400

    Documenatation v1- gemeric and backend

---
+++ mintlify.config.ts
@@ -0,0 +1,13 @@
+export default {
+    docs: [
+      {
+        name: "OceanPulse",
+        href: "/introduction"
+      },
+      {
+        name: "Species Endpoints",
+        href: "/api-reference/species-endpoints"
+      }
+    ]
+  }
+  
